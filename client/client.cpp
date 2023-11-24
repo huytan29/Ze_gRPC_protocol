32ec689e33e0df56f6a4c46413f50caa8dc2a206
huytan29 <huytan29@gmail.com>
2023-11-24 09:43:44 +0700

    update session_service

--- client/client.cpp
+++ client/client.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <memory>
 #include <string>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+
 #include <grpcpp/grpcpp.h>
 #include "../protos/file_transfer_lib/file_transfer.grpc.pb.h"
 #include "../protos/file_transfer_lib/file_transfer.pb.h"
+#include "../protos/session_lib/session.grpc.pb.h"
+#include "../protos/session_lib/session.pb.h"
+#include "class_session.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
 using grpc::Status;
 using filetransfer::FileRequest;
 using filetransfer::FileResponse;
 using filetransfer::FileTransfer;
+using session::Greeter;
+using session::Reply;
+using session::Request;
+using session::SessionReply;
+
+ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
+
 
 class FileTransferClient {
 public:
@@ -37,7 +54,52 @@ private:
     std::unique_ptr<FileTransfer::Stub> stub;
 };
 
-int main() {
+session_an Test;
+class SessionClient {
+public:
+  SessionClient(std::shared_ptr<Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  void SessionCall() {
+    ClientContext context;
+    Request request;
+    std::shared_ptr<ClientReader<SessionReply>> reader(
+        stub_->SessionInfor(&context, request));
+
+    SessionReply reply;
+    while (reader->Read(&reply)) {
+      Test.init(reply.key(), reply.session_id());
+      Test.addModel(reply.filename(), reply.data());
+    }
+
+    Status status = reader->Finish();
+    if (status.ok()) {
+      std::cout << "Call Session rpc succeeded." << std::endl;
+    } else {
+      std::cout << "Call Session rpc failed: " << status.error_message() << std::endl;
+    }
+  }
+
+private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
+
+void GetSession(int argc, char **argv) {
+  absl::ParseCommandLine(argc, argv);
+  std::string target_str = absl::GetFlag(FLAGS_target);
+
+  SessionClient sessionClient(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+
+  sessionClient.SessionCall();
+  Test.showModel();
+}
+
+int main(int argc, char **argv) {
+    // Rest of the code remains the same as before
+    GetSession(argc, argv);
+
+    // Existing file transfer client code
     std::string server_address("192.168.56.1:50051"); // Replace with the server address
     FileTransferClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
 
@@ -52,3 +114,4 @@ int main() {
 
     return 0;
 }
+
--- server/server.cpp
+++ server/server.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+
 #include <grpcpp/grpcpp.h>
 #include "../protos/file_transfer_lib/file_transfer.grpc.pb.h"
 #include "../protos/file_transfer_lib/file_transfer.pb.h"
-#include <fstream>
+#include "../protos/session_lib/session.grpc.pb.h"
+#include "../protos/session_lib/session.pb.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerAsyncWriter;
+using grpc::ServerWriter;
 using grpc::Status;
 using filetransfer::FileRequest;
 using filetransfer::FileResponse;
 using filetransfer::FileTransfer;
+using session::Greeter;
+using session::Reply;
+using session::Request;
+using session::SessionReply;
 
 class FileTransferServiceImpl final : public FileTransfer::Service {
     public:
@@ -37,19 +54,70 @@ class FileTransferServiceImpl final : public FileTransfer::Service {
         }
 };
 
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Greeter::Service
+{
+  public:
+  Status SayHello(ServerContext *context, const Request *request,
+                  Reply *reply) override
+  {
+    std::string prefix("Get session ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
+
+  Status SessionInfor(ServerContext *context, const Request *request,
+                      ServerWriter<SessionReply> *reply) override
+  {
+    // std::string prefix("mod.txt");
+    // reply->set_session_id("123-4556-789");
+    // reply->set_data("2 3 1 2 3 4 5 6");
+    // reply->set_filename(prefix);
+    session::SessionReply newsession;
+    newsession.set_session_id("sessionId-123-456-789");
+    newsession.set_key("key-123456");
+    session_list.push_back(newsession);
+    reply->Write(newsession);
+    newsession.set_filename("w3.txt");
+    newsession.set_data("2 3 1.2 2.3 3.4 4.5 5.6 6.7");
+    session_list.push_back(newsession);
+    newsession.set_filename("w2.txt");
+    newsession.set_data("2 3 1 11 1 1 1 1");
+    session_list.push_back(newsession);
+    newsession.set_filename("w1.txt");
+    newsession.set_data("2 3 1 11 1 1 1 1");
+    session_list.push_back(newsession);
+    for (const SessionReply& f : session_list)
+    {
+      f.filename();
+      f.session_id();
+      f.data();
+      f.key();
+      reply->Write(f);
+    }
+    session_list.clear();
+    return Status::OK;
+  }
+  private:
+  std::vector<SessionReply> session_list;
+};
+
 void RunServer() {
     std::string server_address("0.0.0.0:50051");
-    FileTransferServiceImpl service;
+    GreeterServiceImpl greeterService;
+    FileTransferServiceImpl fileTransferService;
 
     ServerBuilder builder;
 
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
+    builder.RegisterService(&greeterService); // Register Greeter service
+    builder.RegisterService(&fileTransferService); // Register FileTransfer service
 
     std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "server listening on " << server_address << std::endl;
+    std::cout << "Server listening on " << server_address << std::endl;
     server->Wait();
-} 
+}
+
 
 int main() {
     RunServer();
